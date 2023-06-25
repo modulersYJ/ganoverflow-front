caa8134112449279fde682b7e8a5b59a6b003663
horz.d <he1236@ajou.ac.kr>
2023-06-26 08:45:54 +0900

    env: host 끝 /중복 제거로 중복/ 문제 제거

--- ganoverflow-next/src/app/api/env/HOST.ts
+++ ganoverflow-next/src/app/api/env/HOST.ts
@@ -2,4 +2,4 @@
 
 // export const HOST = LOCAL_API_HOST;
 // export const HOST = "http://127.0.0.1:3100";
-export const HOST = "https://api.ganoverflow.com/";
+export const HOST = "https://api.ganoverflow.com";
