02e59ef03457507cf2e1822283344100409ae7d2
horiz.d <65459616+ABizCho@users.noreply.github.com>
2023-08-05 02:39:12 +0900

    feat-chat: sidebar 레이아웃 개발용 간이  스타일 -> black 기반 개선

--- ganoverflow-next/src/app/chat/components/chatSideBar.tsx
+++ ganoverflow-next/src/app/chat/components/chatSideBar.tsx
@@ -44,23 +44,25 @@ IChatSideBarProps) {
 
   return (
     <div className="ChatPageCont">
-      <div className="ChatSideBar fixed hidden md:block md:top-[68px] left-0 w-60 h-full bg-gray-800">
-        <div className="contentsBox h-full w-11/12 m-3 bg-red-100">
-          <div className="sideHeader bg-red-600 h-14 w-full flex justify-center gap-2">
+      <div className="ChatSideBar fixed hidden md:block md:top-[68px] left-0 w-60 h-full">
+        <div className="contentsBox h-full w-11/12 bg-black">
+          <div className="sideHeader bg-black h-14 w-full flex justify-center gap-2 ">
             <button
-              className="text-white mt-2 borderBtn w-3/5 h-4/6 bg-indigo-900 rounded text-sm"
+              className="text-white mt-2 borderBtn w-3/5 h-4/6 border rounded text-sm"
               onClick={onClickNewChatBtn}
             >
               New Chat
             </button>
             <button
-              className="text-white mt-2 borderBtn w-1/5 h-4/6 bg-indigo-900 rounded"
+              className="text-white mt-2 borderBtn w-1/5 h-4/6 border rounded"
               onClick={onClickNewFolderBtn}
             >
               <CreateNewFolderIcon sx={{ fontSize: "14px" }} />
             </button>
           </div>
-          <div className="plain-text text-black py-2">my posts</div>
+          <div className="text-xs text-left my-3 ml-5 text-gray-400">
+            My Posts
+          </div>
           <div className="list-container overflow-y-auto h-screen pb-[200px]">
             <FolderFileNoOrderDND loadThisChatHandler={loadThisChatHandler} />
           </div>
