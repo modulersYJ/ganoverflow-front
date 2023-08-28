cc05b23bb53c32bb3288244180841b0b7ebb27cb
horiz.d <65459616+ABizCho@users.noreply.github.com>
2023-08-29 03:33:17 +0900

    feat-plp: aside 추가, 레이아웃 조금 수정

--- ganoverflow-next/src/app/posts/page.tsx
+++ ganoverflow-next/src/app/posts/page.tsx
@@ -32,58 +32,59 @@ export default async function PostPage({
 
   return (
     <div className="">
-      <div className="post-chatpostName text-xl my-7">
-        <h2 className="">Chats</h2>
-      </div>
-      <div className="grid">
-        <table className="w-3/5 h-[600px] place-self-center">
-          <thead className="posts-tablehead border border-gray-300 border-x-0">
-            <tr className="whitespace-nowrap">
-              <th className="p-2.5">번호</th>
-              <th className="p-2.5 hidden md:block">카테고리</th>
-              <th className="p-2.5">제목</th>
-              <th className="p-2.5">글쓴이</th>
-              <th className="p-2.5 hidden md:block">작성일</th>
-              <th className="p-2.5">댓글</th>
-              <th className="p-2.5">조회수</th>
-              <th className="p-2.5 hidden md:block">추천</th>
-            </tr>
-          </thead>
-          <tbody>
-            {allPosts.posts.length === 10 ? (
-              allPosts.posts.map((post: any, id: number) => (
-                <tr
-                  className="border border-x-0 border-spacing-2 border-zinc-500 hover:bg-slate-600"
-                  key={id}
-                >
-                  <td className="py-1">{post?.chatPostId}</td>
-                  <td className="py-1 hidden md:table-cell">
-                    {post?.categoryName?.categoryName ?? "카테고리업슴"}
-                  </td>
-                  <td className="py-1">
-                    <Link href={`/posts/${post?.chatPostId}`}>
-                      {post?.chatpostName}
-                    </Link>
-                  </td>
-                  <td className="py-1">{post?.user?.nickname ?? ""}</td>
-                  <td className="py-1 hidden md:table-cell">
-                    {parseDateWithSeconds(post?.createdAt)}
-                  </td>
-                  <td className="py-1">{post?.comments?.length}</td>
-                  <td className="py-1">{post?.viewCount}</td>
-                  <td className="py-1 hidden md:table-cell">
-                    {post?.stars.reduce(
-                      (acc: number, curr: any) => acc + curr.value,
-                      0
-                    )}
-                  </td>
-                </tr>
-              ))
-            ) : (
-              <></>
-            )}
-          </tbody>
-        </table>
+      <div className="flex flex-row justify-center gap-4">
+        <div className="grid w-3/5">
+          <table className="board w-full h-[600px] place-self-center">
+            <thead className="posts-tablehead border border-gray-300 border-x-0">
+              <tr className="whitespace-nowrap">
+                <th className="p-2.5">번호</th>
+                <th className="p-2.5 hidden md:block">카테고리</th>
+                <th className="p-2.5">제목</th>
+                <th className="p-2.5">글쓴이</th>
+                <th className="p-2.5 hidden md:block">작성일</th>
+                <th className="p-2.5">댓글</th>
+                <th className="p-2.5">조회수</th>
+                <th className="p-2.5 hidden md:block">추천</th>
+              </tr>
+            </thead>
+            <tbody>
+              {allPosts.posts.length === 10 ? (
+                allPosts.posts.map((post: any, id: number) => (
+                  <tr
+                    className="border border-x-0 border-spacing-2 border-zinc-500 hover:bg-slate-600"
+                    key={id}
+                  >
+                    <td className="py-1">{post?.chatPostId}</td>
+                    <td className="py-1 hidden md:table-cell">
+                      {post?.categoryName?.categoryName ?? "카테고리업슴"}
+                    </td>
+                    <td className="py-1">
+                      <Link href={`/posts/${post?.chatPostId}`}>
+                        {post?.chatpostName}
+                      </Link>
+                    </td>
+                    <td className="py-1">{post?.user?.nickname ?? ""}</td>
+                    <td className="py-1 hidden md:table-cell">
+                      {parseDateWithSeconds(post?.createdAt)}
+                    </td>
+                    <td className="py-1">{post?.comments?.length}</td>
+                    <td className="py-1">{post?.viewCount}</td>
+                    <td className="py-1 hidden md:table-cell">
+                      {post?.stars.reduce(
+                        (acc: number, curr: any) => acc + curr.value,
+                        0
+                      )}
+                    </td>
+                  </tr>
+                ))
+              ) : (
+                <></>
+              )}
+            </tbody>
+          </table>
+        </div>
+
+        <aside className="w-1/5 bg-red-400"></aside>
       </div>
       <div className="pagination-wrapper">
         <Pagination currentPage={currentPage} totalPage={totalPage} />
