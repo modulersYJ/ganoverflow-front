e5d2c57ad3b363259acce94e4a7953ef23ffa337
horiz.d <65459616+ABizCho@users.noreply.github.com>
2023-08-31 05:36:11 +0900

    fix-style: chat - textarea 엔터 & 컬러 전환 부드럽게 스타일 수정

--- ganoverflow-next/src/app/chat/ChatMain/index.tsx
+++ ganoverflow-next/src/app/chat/ChatMain/index.tsx
@@ -1,8 +1,6 @@
 "use client";
 import { useState } from "react";
 import { IChatMainProps } from "@/interfaces/IProps/chat";
-import { useRecoilValue } from "recoil";
-import { TIsSigned, isSignedState } from "@/atoms/sign";
 
 import {
   ChatContent,
@@ -11,14 +9,11 @@ import {
   PromptConsole,
   SaveChatModal,
 } from "./components";
-import { LoginBoxModal } from "@/app/accounts/login/LoginBoxModal";
 
 const ChatMain = ({ onClickNewChatBtn, scrollRef }: IChatMainProps) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [categories, setCategories] = useState<{ categoryName: string }[]>([]);
   const [isNowAnswering, setIsNowAnswering] = useState<boolean>(false);
-
-  const isSigned = useRecoilValue(isSignedState);
 
   return (
     <div className="flex flex-col h-full ">
