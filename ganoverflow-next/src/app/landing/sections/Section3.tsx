3d4be0bce45e4210850d3bcdf6bce73cebe27157
horiz.d <65459616+ABizCho@users.noreply.github.com>
2023-09-04 05:43:38 +0900

    랜딩: 섹션3 문구수정, 섹션2 대시보드 컴포넌트화 및 수정, 유의미 더미데이터 정의

--- ganoverflow-next/src/app/landing/sections/Section3.tsx
+++ ganoverflow-next/src/app/landing/sections/Section3.tsx
@@ -28,7 +28,7 @@ const Section3_2 = () => {
     >
       <div className="sec-3-2 flex flex-col gap-10 mx-auto font-oswald w-full h-full py-[5%] sm:py-16 px-[0%] sm:px-[15%]">
         <h1 className="text-center !text-xl sm:!text-3xl md:!text-3xl lg:!text-5xl">
-          <p>유용한 AI 대화내역,</p>
+          <p className="text-secondary">유용한 AI 대화내역,</p>
           <p>간편하게 기록하고 관리하세요</p>
         </h1>
         <Paragraph>
@@ -38,15 +38,15 @@ const Section3_2 = () => {
         </Paragraph>
         <Paragraph>
           다시 같은 문제에 직면했을 때, 내가 이미 도출해냈던 잘 기록된 검증된
-          답변을 조회할지, 똑같은 해결책에 도달하기 위한 질의의 시행착오를 다시
+          답변을 조회 할지, 똑같은 해결책에 도달하기 위한 질의의 시행착오를 다시
           한번 거칠지, 저울질 해보지 않아도 무엇이 더 효율적인지 알 수 있습니다.
         </Paragraph>
         <Paragraph>
-          우리는 불친절한 생성형 AI서비스의 기록 및 관리체계 때문에 재사용
-          가능한 양질의 답변을 도출하고서도, 휘발성으로 사용하고 잊어버리기
-          일쑤입니다. GANoverflow 팀은 당신이 도출해낸 AI의 답변을 체계적으로
-          기록, 관리할 수 있는 수단을 지원하고, 당신의 생산성을 높이기 위한
-          비전을 발전시키고 있습니다.
+          GANoverflow 팀은 당신이 도출해낸{" "}
+          <span className="text-secondary">
+            AI의 답변을 체계적으로 기록, 관리 할 수 있는 수단을 지원
+          </span>
+          하고, 당신의 생산성을 높이기 위한 비전을 발전시키고 있습니다.
         </Paragraph>
       </div>
     </div>
